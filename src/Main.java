79ceee8ee962e99284fce257d72c6f0040e8a802
Марта Труш <trusmarta804@gmail.com>
2024-02-29 16:31:40 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // 1 Завдання
+        int a = 12;
+        int b = 5;
+        double c = ((double) a/b);
+        System.out.println("Результат ділення: " +c );
+        int d = a%b;
+        System.out.println("Результат залишку від ділення: " +d );
+
+
+        // 2 Завдання
+        int a1 = 54;
+        int b1 = a1/10;
+        int c1 = a1%10;
+        int d1 = b1+c1;
+        System.out.println("Сума цифр двозначного числа: " +d1 );
+
+
+        // 3 Завдання
+        double a2 = 4.3;
+        int b2 = (int) (a2 + 0.5);
+        System.out.println("Округлене число: " + b2);
+
+
+
+    }
+}
